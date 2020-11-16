427e33ea53719d690bfd707c9f582e57eac76f97
Rubens Kleinschmidt Júnior <36218162+RubensKj@users.noreply.github.com>
2020-11-16 20:37:18 -0300

    Interpreter Design Pattern - Exercise sqrt

--- src/main/java/com/rubenskj/pattern/interpreter/Programa.java
+++ src/main/java/com/rubenskj/pattern/interpreter/Programa.java
@@ -5,7 +5,7 @@ public class Programa {
     public static void main(String[] args) {
 
         Expressao esquerda = new Subtracao(new Multiplicacao(new Numero(5), new Numero(7)), new Numero(5));
-        Expressao direita = new Soma(new Divisao(new Numero(6), new Numero(2)), new Numero(10));
+        Expressao direita = new Soma(new Divisao(new Numero(6), new Numero(2)), new RaizQuadrada(new Numero(16)));
 
         Expressao soma = new Soma(esquerda, direita);
 
